6b07f8191677c9801f029b2b487f0e9ab1a27854
Rui Zhu <rzhu72@uwo.ca>
2021-07-30 14:48:29 -0400

    7/30/2021 push

---
+++ leetcode/getLucky.go
@@ -0,0 +1,16 @@
+package leetcode
+func getLucky(s string, k int) (sum int) {
+	for _,b := range s{
+		b-='a'-1
+		sum+=int(b/10+b%10);
+
+	}
+	for k--;k>0&&sum>9;k--{
+		s:=sum
+		for sum=0;s>0;s/=10{
+			sum+=s%10
+		}
+
+	}
+	return
+}
